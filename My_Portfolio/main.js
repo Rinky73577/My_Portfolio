11938eea71bf2f3df10dea82f46e47d2e39506a3
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2025-01-27 22:39:13 +0530

    Update index.html

--- My_Portfolio/main.js
+++ My_Portfolio/main.js
@@ -20,9 +20,27 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             navLinks.forEach(link => {
                 link.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                // document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
             });
         }
     });
 };
+
+
+// // Get the toggle button and navbar
+// const menuIcon = document.getElementById("menu-icon");
+// const navbar = document.querySelector(".navbar");
+
+// // Add event listener to toggle the navbar visibility
+// menuIcon.addEventListener("click", () => {
+//     console.log('hh')
+//     navbar.classList.toggle("show");
+// });
+function toggleMenu() {
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("show");
+}
+
+
 
